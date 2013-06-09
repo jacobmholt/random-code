e1362f9cc6a8760ccb70a9e9830b9b10f611e7ac
Jacob Holt <jacob.matthew.holt@gmail.com>
2013-06-09 20:13:28 +0100

    v1.3

--- countdown.c
+++ countdown.c
@@ -8,9 +8,10 @@ int num ;
 num = 0;
 printf("What is your number?") ;
 scanf("%d", &num);
+num = (num - 1);
 for (;num > 0;num--)
 {
-printf("%d\n", &num);
+printf("%d\n", (int)num);
 }
 
 return 0 ;
