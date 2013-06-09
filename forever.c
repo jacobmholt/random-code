698ea3a768c98aaff14cacb87c1cfd1aedc79425
Jacob Holt <jacob.matthew.holt@gmail.com>
2013-06-09 15:38:34 +0100

    v1.0

---
+++ forever.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+
+int main(void)
+
+{
+for (;;) {
+printf ("This is going to go on for like forever, man\n") ;
+
+}
+
+return 0 ;
+
+}
---
+++ variables.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+
+{
+int i;
+int z;
+printf("What is i? ");
+scanf("%d", &i);
+printf("i is equal to %d\n", i);
+z = i - 1;
+printf("It is also larger than %d\n", z);
+
+return 0;
+}
