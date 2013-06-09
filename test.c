0520994fc4a43b41b8b36487541a4cbf4f7fc226
Jacob Holt <jacob.matthew.holt@gmail.com>
2013-06-09 20:32:26 +0100

    v1.5

---
+++ test.c
@@ -0,0 +1,27 @@
+int main(void)
+
+
+
+{
+int num, result = 0;
+scanf("%d", &num); // read the number from the keyboard
+
+
+
+for(i = 1; i <= num; i++) { // compute the result
+
+
+
+}
+
+
+
+printf("%d\n", result); // output the result
+
+
+
+return 0;
+
+
+
+}
